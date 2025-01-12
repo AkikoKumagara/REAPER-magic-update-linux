d7b39966ef43e85d959b10a56a4aafbd821a49c3
Akiko <sophie-bear@solsticegamestudios.com>
2025-01-12 09:58:28 -0500

    Update reapermagicupdate.sh
    Remove -rf in favor of a simple recursive removal instead.

--- reapermagicupdate.sh
+++ reapermagicupdate.sh
@@ -2,4 +2,4 @@ cd ~/Downloads
 curl -sLo - "https://reaper.fm/$(curl -s "https://www.reaper.fm/download.php" | grep -o -m 1 "files/.*\.x/reaper.*_linux_x86_64.tar.xz")" | tar xJ
 cd reaper_linux_x86_64
 sh ./install-reaper.sh
-cd .. && rm -rf reaper_linux_x86_64
+cd .. && rm -r reaper_linux_x86_64
